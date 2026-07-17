18d30a8920baa6312da589d47cdfdc51298fa7b7
Cameron Beckwith <cameronbeckwith@gmail.com>
2026-07-17 14:20:47 -0700

    starting work on the quaternion library

---
+++ include/QMath.h
@@ -0,0 +1,18 @@
+#ifndef QMATH_H
+#define QMATH_H
+
+class QMath
+{
+public:
+	Vec4<float> doRotation(Vec4<float> quat, Vec4<float> normalVector)
+	{
+
+	}
+
+	Vec4<float> dotProduct(Vec4<float> leftQuat, Vec4<float> rightQuat)
+	{
+		Vec4<float> product = {leftQuat.m_x}
+	}
+};
+
+#endif
